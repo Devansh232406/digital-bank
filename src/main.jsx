995a5a0988371adf0a1b8ff240a771e49ece6dc9
Ultimate One <devansh4300@gmail.com>
2026-08-01 17:58:28 +0530

    Set up global styles and design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
+import "./styles/variables.css";
+import "./styles/global.css";
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
