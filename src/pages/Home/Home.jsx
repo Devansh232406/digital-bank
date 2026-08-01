db1f0b183a7be6eca311e83fe8695e8b106fc814
Ultimate One <devansh4300@gmail.com>
2026-08-01 18:07:55 +0530

    Create navbar and layout component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,11 @@
-function Home(){
-    return <h1>Home Page</h1>;
+import Layout from "../../components/layout/Layout";
+
+function Home() {
+  return (
+    <Layout>
+      <h1>Welcome to DigitalBank</h1>
+    </Layout>
+  );
 }
+
 export default Home;
